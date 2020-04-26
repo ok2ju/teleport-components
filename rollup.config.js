ab5b1d05512955de4c8812faf89502724ade8870
ok2ju <soulSpringg@gmail.com>
2020-04-26 13:57:23 +0300

    Switch to `wessberg` ts rollup plugin
    wessberg ts rollup plugin allow us to expose type definitions

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 import path from 'path'
 import resolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
+import typescript from '@wessberg/rollup-plugin-ts'
 import commonjs from '@rollup/plugin-commonjs'
 
+// Files relative to current working directory
 const PACKAGE_ROOT_PATH = process.cwd()
 const pkg = require(path.join(PACKAGE_ROOT_PATH, 'package.json'))
-const tsconfigPath = path.join(PACKAGE_ROOT_PATH, 'tsconfig.build.json')
 const inputFile = path.join(PACKAGE_ROOT_PATH, 'src/index.ts')
 
 export default {
@@ -27,9 +27,7 @@ export default {
   ],
   plugins: [
     resolve(),
-    typescript({
-      tsconfig: tsconfigPath
-    }),
+    typescript(),
     commonjs()
   ]
 }
